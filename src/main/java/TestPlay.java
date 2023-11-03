633f4194936f4ed4b1956ab325dd6942da630344
Mat Esquivel <mat.esquivel.qa@gmail.com>
2023-11-03 15:58:27 -0300

    Playwright Start Project

---
+++ src/main/java/TestPlay.java
@@ -0,0 +1,24 @@
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import models.HomePage;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import java.util.*;
+
+public class TestPlay {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+
+            HomePage homePage = new HomePage(page);
+            homePage.navigate("https://opencart.abstracta.us/");
+            homePage.perfPruchaseInCart();
+            homePage.assertPurchase();
+
+        }
+    }
+}
---
+++ src/main/java/models/HomePage.java
@@ -0,0 +1,61 @@
+package models;
+
+
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.LoadState;
+
+import java.util.List;
+
+public class HomePage {
+    private final Page page;
+    private final Locator search;
+
+    public HomePage(Page page) {
+        this.page = page;
+        this.search = page.locator("a");
+
+    }
+
+    public void navigate(String url){
+        page.navigate(url);
+    }
+
+    public void perfPruchaseInCart(){
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Components").setExact(true)).click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Show All Components")).click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Monitors (2)").setExact(true)).click();
+        page.getByText("Samsung SyncMaster 941BW").click();
+        page.getByLabel("Qty").click();
+        page.getByLabel("Qty").fill("2");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to Cart")).click();
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName(" 2 item(s) - $484.00")).click();
+        page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" View Cart")).click();
+
+        page.waitForLoadState(LoadState.LOAD);
+    }
+
+    public void assertPurchase(){
+        String totalText = page.querySelectorAll(".text-right").get(1).textContent();
+        System.out.println("The total is: " + totalText);
+
+        String unitText = page.querySelectorAll(".text-right").get(13).textContent();
+        System.out.println("The unit price is: " + unitText);
+
+        List<ElementHandle> elements = page.querySelectorAll(".text-right");
+
+        for (ElementHandle element : elements){
+            String elementText = element.textContent();
+            System.out.println("Element is:" + elementText);
+        }
+
+        page.querySelector("[data-original-title='Update']").click();
+
+        page.waitForSelector(".alert-success", new Page.WaitForSelectorOptions());
+
+        String successMes = page.querySelector(".alert-success").textContent();
+        System.out.println(successMes);
+    }
+}
